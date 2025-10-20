541743d11fedc724e88a267697f61d4358520abc
hjoncour <hugo.joncour.k2o@gmail.com>
2025-10-19 21:59:46 -0400

    feature/page

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,11 +1,12 @@
 use yew::prelude::*;
+use crate::components::ascii_animation::AsciiAnimation;
 
 #[function_component(HomePage)]
 pub fn home_page() -> Html {
     html! {
         <div class="container">
             <h1>{"Cascii Studio"}</h1>
-            <p>{"Click on the Tauri and Yew logos to learn more."}</p>
+            <AsciiAnimation frame_folder="loop_project" fps={30} />
         </div>
     }
 }

2075b2d876959683fa004b57863966fd45ff05b2
hjoncour <hugo.joncour.k2o@gmail.com>
2025-10-22 22:20:38 -0400

    fix/icon-and-sidebar-styles

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -5,6 +5,8 @@ use yew_icons::{Icon, IconId};
 pub struct SidebarProps {
     /// Callback when a nav item is clicked; argument is a simple route token
     pub on_navigate: Callback<&'static str>,
+    pub current_page: String,
+    pub has_active_project: bool,
 }
 
 #[function_component(Sidebar)]
@@ -14,15 +16,39 @@ pub fn sidebar(props: &SidebarProps) -> Html {
         Callback::from(move |_| cb.emit(route))
     };
 
+    let get_btn_class = |route: &'static str, current_page: &str| {
+        if current_page == route {
+            "nav-btn active"
+        } else {
+            "nav-btn"
+        }
+    };
+
     html! {
         <aside class="sidebar">
-            <button class="nav-btn" title="Project" type="button" onclick={nav("project")}>   <Icon icon_id={IconId::LucideBrush}        width={"28"} height={"28"} /></button>
-            <button class="nav-btn" title="Home" type="button" onclick={nav("home")}>      <Icon icon_id={IconId::LucideHome}         width={"28"} height={"28"} /></button>
-            <button class="nav-btn" title="New" type="button" onclick={nav("new")}>        <Icon icon_id={IconId::LucidePlus}         width={"28"} height={"28"} /></button>
-            <button class="nav-btn" title="Open" type="button" onclick={nav("open")}>      <Icon icon_id={IconId::LucideFolderOpen}  width={"28"} height={"28"} /></button>
-            <button class="nav-btn" title="Settings" type="button" onclick={nav("settings")}><Icon icon_id={IconId::LucideSettings}    width={"28"} height={"28"} /></button>
-            <button class="nav-btn" title="Library" type="button" onclick={nav("library")}> <Icon icon_id={IconId::LucideLibrary}     width={"28"} height={"28"} /></button>
-            <button class="nav-btn" title="Sponsor" type="button" onclick={nav("sponsor")}> <Icon icon_id={IconId::LucideHeart}       width={"28"} height={"28"} /></button>
+            if props.has_active_project {
+                <button class={get_btn_class("project", &props.current_page)} title="Project" type="button" onclick={nav("project")}>
+                    <Icon icon_id={IconId::LucideBrush} width={"28"} height={"28"} />
+                </button>
+            }
+            <button class={get_btn_class("home", &props.current_page)} title="Home" type="button" onclick={nav("home")}>
+                <Icon icon_id={IconId::LucideHome} width={"28"} height={"28"} />
+            </button>
+            <button class={get_btn_class("new", &props.current_page)} title="New" type="button" onclick={nav("new")}>
+                <Icon icon_id={IconId::LucidePlus} width={"28"} height={"28"} />
+            </button>
+            <button class={get_btn_class("open", &props.current_page)} title="Open" type="button" onclick={nav("open")}>
+                <Icon icon_id={IconId::LucideFolderOpen} width={"28"} height={"28"} />
+            </button>
+            <button class={get_btn_class("settings", &props.current_page)} title="Settings" type="button" onclick={nav("settings")}>
+                <Icon icon_id={IconId::LucideSettings} width={"28"} height={"28"} />
+            </button>
+            <button class={get_btn_class("library", &props.current_page)} title="Library" type="button" onclick={nav("library")}>
+                <Icon icon_id={IconId::LucideLibrary} width={"28"} height={"28"} />
+            </button>
+            <button class={get_btn_class("sponsor", &props.current_page)} title="Sponsor" type="button" onclick={nav("sponsor")}>
+                <Icon icon_id={IconId::LucideHeart} width={"28"} height={"28"} />
+            </button>
         </aside>
     }
 }

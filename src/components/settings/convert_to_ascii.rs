2e6f38c1397bdb7e0bd3ec1d4668888028abb1cc
Hugo Joncour <hugo.joncour.k2o@gmail.com>
2026-01-23 12:46:00 -0500

    feature(frames): create color frames (#52)

--- src/components/settings/convert_to_ascii.rs
+++ src/components/settings/convert_to_ascii.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use yew_icons::{Icon, IconId};
 use crate::pages::project::SourceContent;
 use wasm_bindgen::prelude::*;
 use serde::{Deserialize, Serialize};
@@ -13,6 +14,7 @@ struct ConvertToAsciiRequest {
     fps: Option<u32>,
     project_id: String,
     source_file_id: String,
+    color: bool,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -59,6 +61,16 @@ pub struct ConvertToAsciiProps {
 
 #[function_component(ConvertToAscii)]
 pub fn convert_to_ascii(props: &ConvertToAsciiProps) -> Html {
+    // State for color generation toggle
+    let generate_colors = use_state(|| true);
+
+    let on_toggle_colors = {
+        let generate_colors = generate_colors.clone();
+        Callback::from(move |_| {
+            generate_colors.set(!*generate_colors);
+        })
+    };
+
     let on_toggle = {
         let on_toggle_collapsed = props.on_toggle_collapsed.clone();
         Callback::from(move |_| {
@@ -125,12 +137,14 @@ pub fn convert_to_ascii(props: &ConvertToAsciiProps) -> Html {
         let columns = props.columns;
         let fps = props.fps;
         let project_id = props.project_id.clone();
+        let generate_colors = generate_colors.clone();
         let on_is_converting_change = props.on_is_converting_change.clone();
         let on_conversion_message_change = props.on_conversion_message_change.clone();
         let on_error_message_change = props.on_error_message_change.clone();
         let on_refresh_frames = props.on_refresh_frames.clone();
 
         Callback::from(move |_| {
+            let color = *generate_colors;
             if let Some(source) = &selected_source {
                 let file_path = source.file_path.clone();
                 let source_file_id = source.id.clone();
@@ -145,7 +159,7 @@ pub fn convert_to_ascii(props: &ConvertToAsciiProps) -> Html {
 
                 wasm_bindgen_futures::spawn_local(async move {
                     let invoke_args = ConvertToAsciiInvokeArgs {
-                        request: ConvertToAsciiRequest {file_path, luminance, font_ratio, columns, fps: Some(fps), project_id: project_id_clone.clone(), source_file_id}
+                        request: ConvertToAsciiRequest {file_path, luminance, font_ratio, columns, fps: Some(fps), project_id: project_id_clone.clone(), source_file_id, color}
                     };
 
                     let args = serde_wasm_bindgen::to_value(&invoke_args).unwrap();
@@ -227,13 +241,22 @@ pub fn convert_to_ascii(props: &ConvertToAsciiProps) -> Html {
                                 }
                             </div>
 
-                            <button class="btn-convert" disabled={props.is_converting || props.selected_source.is_none()} onclick={on_convert_click}>
-                                if props.is_converting {
-                                    {"Converting..."}
-                                } else {
-                                    {"Convert to ASCII"}
-                                }
-                            </button>
+                            <div class="convert-actions">
+                                <button class={classes!("color-toggle-btn", (*generate_colors).then_some("active"))} onclick={on_toggle_colors} title={if *generate_colors { "Color generation enabled" } else { "Color generation disabled" }}>
+                                    if *generate_colors {
+                                        <Icon icon_id={IconId::LucideBrush} width={"18"} height={"18"} />
+                                    } else {
+                                        <Icon icon_id={IconId::LucideXCircle} width={"18"} height={"18"} />
+                                    }
+                                </button>
+                                <button class="btn-convert" disabled={props.is_converting || props.selected_source.is_none()} onclick={on_convert_click}>
+                                    if props.is_converting {
+                                        {"Converting..."}
+                                    } else {
+                                        {"Convert to ASCII"}
+                                    }
+                                </button>
+                            </div>
 
                             {
                                 if let Some(msg) = &props.conversion_message {
